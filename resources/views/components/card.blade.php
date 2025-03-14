9f70075f139638682a7cea35b495d8c9914b06b4
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-03-14 11:55:08 +0100

    Add Vite asset compilation and improve card layout styling

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="card">
+<div class="card w-25 text-center p-3">
     <h3>{{$title}}</h3>
-    <img src={{$img}} alt="{{$title}}-image">
+    <img src={{$img}} alt="{{$title}}-image" class="w-50 mx-auto">
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,13 @@ Home
 @endsection
 
 @section('content')
+<div class="d-flex gap-3 flex-wrap justify-content-center">
     @foreach ($comics as $comic)
         <x-card>
             <x-slot:title>{{$comic["title"]}}</x-slot:title>
             <x-slot:img>{{$comic["thumb"]}}</x-slot:img>
         </x-card>
     @endforeach
+</div>
+
 @endsection
